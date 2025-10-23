563b62d9d25043428c2f3964ed3cbe3cbc359a1b
DanielUtodio <danielletstryagain@gmail.com>
2025-10-23 18:48:29 +0100

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import CustomerLogin from './customer_Auth/CustomerLogin'
-import CustomerSignup from './customer_Auth/CustomerSignup'
-import VendorSignUp from './customer_Auth/VendorSignUp'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import ResetPassword from './auth/Reset'
 const App = () => {
   return (
-    <>
-    <HashRouter>
-      <Routes>
-        <Route path='/' element={<CustomerLogin />} />
-        <Route path='register' element={<CustomerSignup />} />
-        <Route path='register/register-vendor' element={<VendorSignUp />} />
-      </Routes>
-    </HashRouter>
-    </>
+    <div className="app">
+      <ResetPassword />
+    </div>
   )
 }
 
---
+++ src/auth/Reset.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./reset.css"
+
+const ResetPassword = () => {
+    return (
+        <div className='reset'>
+            <article className="article">
+                <header className="header">
+                    <h3>RefillXpress</h3>
+                </header>
+                <form className="form">
+                    <div className="form-heading">
+                        <h1>reset password</h1>
+                        <p>Dont't worry, it happens a lot!</p>
+                    </div>
+                    <div className="input-container">
+                        <label htmlFor="email">email address</label>
+                        <div className="input-div">
+                            <input type="text" placeholder='your email here...' />
+                            
+                        </div>
+                    </div>
+                </form>
+            </article>
+        </div>
+    )
+}
+
+export default ResetPassword
